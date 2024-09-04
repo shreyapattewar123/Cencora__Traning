f9a25b4e70a717eb7c3c567779ca25b06171d1a1
shreyapattewar123 <shreyapattewar14@gmail.com>
2024-09-04 12:07:57 +0000

    files added

---
+++ src/main/java/com/example/demo/CiCdexample2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdexample2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdexample2Application.class, args);
+	}
+
+}
